a6d58b3f2851caea11d4b550a0b0f3078c40c135
Quadspace <mike@quadspace.us>
2025-05-20 19:53:08 -0400

    Update types.ts

--- pinecone-assistant/src/app/types.ts
+++ pinecone-assistant/src/app/types.ts
@@ -1,8 +1,12 @@
-declare module 'extended-eventsource' {
-  export class EventSource {
-    constructor(url: string, options: any);
-    onmessage: (event: any) => void;
-    onerror: (error: any) => void;
-    close: () => void;
-  }
+export interface File {
+  id: string;
+  filename: string;
+  purpose: string;
+  bytes: number;
+  created_at: number;
+}
+
+export interface Reference {
+  file_id: string;
+  quote: string;
 }
